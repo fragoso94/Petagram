f69b713ab7a8642ce7c4f7d7fb91f8d51e397911
bacha <bachatero_dany@outlook.es>
2018-11-19 10:37:18 -0600

    Incluyendo RisedButton

--- app/src/main/java/com/example/bacha/petagram/MainActivity.java
+++ app/src/main/java/com/example/bacha/petagram/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.bacha.petagram;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        agregarFab();
+    }
+
+    public void agregarFab(){
+        FloatingActionButton miFab = (FloatingActionButton) findViewById(R.id.fb_icon);
+        miFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Bienvenido", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
